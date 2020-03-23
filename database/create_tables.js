7e368db6280e2b111620787ecbd44e2d48ea701b
Ananya <ananyasheshu@gmail.com>
2020-03-23 18:46:56 +0530

    Changed database schema

--- database/create_tables.js
+++ database/create_tables.js
@@ -1,10 +1,5 @@
 let conn = require("../app/models/connection");
 
-// conn.query("DROP DATABASE blogging_app", err => {
-//   if (err) throw err;
-//   console.log("drop");
-// });
-
 // USERS TABLE
 let create_user_table = `
     CREATE TABLE IF NOT EXISTS users(
@@ -28,6 +23,7 @@ let create_followers_table = `
     CREATE TABLE IF NOT EXISTS followers(
         user_id INT,
         follower_id INT,
+        PRIMARY KEY(user_id, follower_id),
         FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY(follower_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
@@ -46,7 +42,8 @@ let create_collections_table = `
         user_id INT,
         image_url VARCHAR(2048),
         description VARCHAR(2048),
-        user_only BOOLEAN,
+        tags VARCHAR(1024),
+        FULLTEXT(collection_name, tags),
         FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
 `;
@@ -58,17 +55,17 @@ conn.query(create_collections_table, (err, result) => {
 
 // USERS_COLLECTIONS TABLE
 // make user_id and collection_id unique together
-let create_users_collections_table = `
-    CREATE TABLE IF NOT EXISTS users_collections(
-        id INT PRIMARY KEY AUTO_INCREMENT,
+let create_collection_followers_table = `
+    CREATE TABLE IF NOT EXISTS collection_followers(
         user_id INT,
         collection_id VARCHAR(255),
+        PRIMARY KEY(user_id, collection_id),
         FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
 `;
 
-conn.query(create_users_collections_table, (err, result) => {
+conn.query(create_collection_followers_table, (err, result) => {
   if (err) throw err;
   console.log("users_collections table created");
 });
@@ -76,9 +73,9 @@ conn.query(create_users_collections_table, (err, result) => {
 // COLLECTION_AUTHORS
 let create_collection_authors_table = `
     CREATE TABLE IF NOT EXISTS collection_authors(
-      id INT PRIMARY KEY AUTO_INCREMENT,
       collection_id VARCHAR(255),
       author_id INT,
+      PRIMARY KEY(collection_id, author_id),
       FOREIGN KEY(author_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
       FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
@@ -103,6 +100,8 @@ let create_articles_table = `
         kudos_count INT,
         date_created DATE,
         date_updated DATE,
+        tags VARCHAR(1024),
+        FULLTEXT(title, tags),
         FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE ON UPDATE CASCADE
     )
@@ -126,121 +125,6 @@ let create_article_bookmarks_table = `
 conn.query(create_article_bookmarks_table, (err, result) => {
   if (err) throw err;
   console.log("article_bookmarks table created");
-});
-
-// QUESTIONS TABLE
-let create_questions_table = `
-    CREATE TABLE IF NOT EXISTS questions(
-        question_id VARCHAR(255) PRIMARY KEY,
-        collection_id VARCHAR(255),
-        user_id INT,
-        title VARCHAR(2048),
-        content LONGTEXT,
-        date_created DATE,
-        date_updated DATE,
-        FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-conn.query(create_questions_table, (err, result) => {
-  if (err) throw err;
-  console.log("questions table created");
-});
-
-// QUESTION_BOOKMARKS TABLE
-let create_question_bookmarks_table = `
-    CREATE TABLE IF NOT EXISTS question_bookmarks(
-        bookmark_id INT PRIMARY KEY AUTO_INCREMENT,
-        question_id VARCHAR(255),
-        user_id INT,
-        FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(question_id) REFERENCES questions(question_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-
-conn.query(create_question_bookmarks_table, (err, result) => {
-  if (err) throw err;
-  console.log("question_bookmarks table created");
-});
-
-// ANSWERS TABLE
-let create_answers_table = `
-    CREATE TABLE IF NOT EXISTS answers(
-        answer_id VARCHAR(255),
-        question_id VARCHAR(255),
-        user_id INT,
-        content LONGTEXT,
-        date_created DATE,
-        date_updated DATE,
-        FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(question_id) REFERENCES questions(question_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-
-conn.query(create_answers_table, (err, result) => {
-  if (err) throw err;
-  console.log("answers table created");
-});
-
-// TAGS TABLE
-let create_tags_table = `
-    CREATE TABLE IF NOT EXISTS tags(
-        tag_id INT PRIMARY KEY AUTO_INCREMENT,
-        tag_name VARCHAR(500)
-    )
-`;
-
-conn.query(create_tags_table, (err, result) => {
-  if (err) throw err;
-  console.log("tags table created");
-});
-
-// COLLECTIONS_TAGS TABLE
-let create_collections_tags_table = `
-    CREATE TABLE IF NOT EXISTS collections_tags(
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        tag_id INT,
-        collection_id VARCHAR(255),
-        FOREIGN KEY(tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-
-conn.query(create_collections_tags_table, (err, result) => {
-  if (err) throw err;
-  console.log("collections_tags table created");
-});
-
-// ARTICLES_TAGS TABLE
-let create_articles_tags_table = `
-    CREATE TABLE IF NOT EXISTS articles_tags(
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        tag_id INT,
-        article_id VARCHAR(255),
-        FOREIGN KEY(tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(article_id) REFERENCES articles(article_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-
-conn.query(create_articles_tags_table, (err, result) => {
-  if (err) throw err;
-  console.log("articles_tags table created");
-});
-
-// QUESTIONS_TAGS TABLE
-let create_questions_tags_table = `
-    CREATE TABLE IF NOT EXISTS questions_tags(
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        tag_id INT,
-        question_id VARCHAR(255),
-        FOREIGN KEY(tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY(question_id) REFERENCES questions(question_id) ON DELETE CASCADE ON UPDATE CASCADE
-    )
-`;
-
-conn.query(create_questions_tags_table, (err, result) => {
-  if (err) throw err;
-  console.log("questions_tags table created");
 });
 
 // COMMENTS TABLE
