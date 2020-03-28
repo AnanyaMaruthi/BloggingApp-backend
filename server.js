afeaf2b5a9dafee8ed929d7171544d793f718e8e
Ananya <ananyasheshu@gmail.com>
2020-03-28 12:18:55 +0530

    Added static file routes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
